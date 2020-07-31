ca0b7a8a4487e72b7353092ecc9240e0a0047270
dannykustin <dannykustin@gmail.com>
2020-07-31 10:50:03 -0700

    real first version of project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function RoundUpdate (){
+
+  //const CodeWords = ["test1", "test2", "test3"];
+
+  //Stores round value
+  const [roundVal, setRoundVal] = useState("");
+
+  useEffect(() => {
+    console.log();
+  }, [roundVal]);
+
+  //Creates Round Info
+  return(
+    < >
+      <h1> 
+        Round Number: 
+        <input type="number" value={roundVal} onChange={e => setRoundVal(e.target.value)}></input>
+      </h1>
+      
+      <form>
+        <div>Team This Round: GREYSON RULLLLEEZZZZZ</div>
+
+        <label>Red: 
+          <input type="radio" ></input>
+        </label>
+
+        <label>Blue: 
+          <input type="radio" ></input>
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Players In This Round: <input type="number"></input>
+        </label>
+
+        <br />
+
+        <input type="button" value="Submit"></input>
+      </form> 
+
+      <br />
+      
+      <p>Code Word:</p>
+    </>
+  )
+}
+
+
+//Creates buttons to Kill and Save Players
+function CodeSend (roundVal){
+  return(
+    < >
+      <p>Player: </p>
+      <input type="button" value="Kill" onClick={KillCode( {roundVal} )}></input>
+      <br />
+      <br />
+      <input type="button" value="Save" onClick={SaveCode}></input>
+    </>
+  )
+}
+
+//Creates the results
+function Result (){
+  return(
+    < >
+      <p>Points This Round: </p>
+      <p>Total Points:</p>
+    </ >
+  )
+}
+
+function KillCode (number){
+  console.log("Kill Code " + number);
+}
+
+function SaveCode (){
+  console.log("Save Code");
+}
+
+//Compiles everything for final render
+function App() {
+  return(
+    < >
+      <RoundUpdate />
+      <br />
+      <br />
+      <CodeSend />
+      <br />
+      <br />
+      <Result />
+    </ >
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
