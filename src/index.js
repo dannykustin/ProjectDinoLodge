3e5d2a91a7858061461cba5a2f48bf5ae6dc4ee4
dannykustin <dannykustin@gmail.com>
2020-08-17 18:38:43 -0700

    major progress to overall system, added component making child ones for simplicity

--- src/index.js
+++ src/index.js
@@ -3,84 +3,110 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //Creates empty varibles to be used in game
-const PlayerArray = []
-//var playerCreated = 0;
+const playerArray = [
+  {
+    id: 0,
+    name: "Player 1",
+    score: 0, 
+    currentTeam: "Red",
+    previousTeams: [],
+    codeSendLog: [],
+    codeReceiveLog: [],
+  },
+  {
+    id: 1,
+    name: "Player 2",
+    score: 0, 
+    currentTeam: "Blue",
+    previousTeams: [],
+    codeSendLog: [],
+    codeReceiveLog: [],
+  },
 
+];
 
-//Functions used for logic of the game:
-
-//Creates buttons to Kill and Save Players
-function CodeSend (roundVal){
-  return(
-    < >
-      <p>Player: </p>
-      <input type="button" value="Kill" onClick={KillCode}></input> 
-      <input type="button" value="Save" onClick={SaveCode}></input>
-    </>
-  )
-}
-
-//Send Kill code to another player
-function KillCode (){
-  console.log("Kill Code");
-}
-
-//Sends Save code to another player
-function SaveCode (){
-  console.log("Save Code");
-}
-
-//Functions used for creating HTML:
+//Creates code words
+const RedCodeWordsArray = ["red1", "red2", "red3"];
+const BlueCodeWordsArray = ["blue1", "blue2", "blue3"];
 
 //Creates new player and HTML for it
-class CreatPlayerHTMl extends React.Component {
+class CreatePlayerHTMl extends React.Component {
+  
+  //builds the tempary varables to be filled later
   constructor(props) {
     super(props);
     this.state = {
       name: '',
       team: '',
+      arraylength: 0,
     };
 
+    //Reacts way of cnnecting actions to functions I want
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  //Called when new player form submitted
+  //Called when form is changed
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
+    const arraylength = playerArray.length;
 
     this.setState({
       [name]: value
     });
+
   }
 
-  //Called when new player form submitted
+ 
+
+  //Called when teh form is submitted
   handleSubmit(event) {
+
+    //Stops the typical way this would be handled
+    event.preventDefault();
+
+    if(this.state.team === "" || this.state.team === "Random" ){
+      var teamChoose = Math.floor(Math.random() * 2);
+      if(teamChoose === 0){
+        this.state.team = "Red";
+      }
+      else{ 
+        if(teamChoose === 1){
+        this.state.team = "Blue";
+        }
+        else{
+          this.state.team = "Error";
+        }
+      }
+        
+    }
 
     //Builds new player object 
     const newPlayer = {
-      id: PlayerArray.length,
-      name: this.state.name,
-      score: 0, 
-      currentTeam: this.state.team,
-      previousTeams: [],
-      codeSendLog: [],
-      codeReceiveLog: [],
+    id: playerArray.length,
+    name: this.state.name,
+    score: 0, 
+    currentTeam: this.state.team,
+    previousTeams: [],
+    codeSendLog: [],
+    codeReceiveLog: [],
     };
 
+
     //Adds new player to the player array
-    PlayerArray.push(newPlayer);
+    playerArray.push(newPlayer);
 
-    console.log(PlayerArray);
-
-    event.preventDefault();
+    console.log(playerArray);
+    
   }
 
   render() {
     return (
+
       < >
+
         <h3>Create New Player:</h3>
         <form onSubmit={this.handleSubmit}>
           <label>
@@ -100,6 +126,7 @@ class CreatPlayerHTMl extends React.Component {
 
           <input type="submit" value="Submit" />
         </form>
+
       </ >
       
     );
@@ -110,82 +137,147 @@ class CreatPlayerHTMl extends React.Component {
 //Creates Admin Interface
 function AdminHTML (props){
 
-    //Stores round value
-    const [roundVal, setRoundVal] = useState("");
-
-    useEffect(() => {
-      console.log();
-    }, [roundVal]);
 
   return (
     < >
-        <label> 
-          Round Number: 
-          <input type="number" value={roundVal} onChange={e => setRoundVal(e.target.value)}></input>
-        </label>
-        <div> Players In This Round: {PlayerArray.length} </div>
+        <p> Round Number: {RedCodeWordsArray[0].score} </p>
+        <div> Players In This Round: {playerArray.length} </div>
 
         <br></br>
 
-        <CreatPlayerHTMl />
+        <CreatePlayerHTMl />
+    </ >
+  )
+}
+
+
+//Send Kill code to another player
+function KillCode (){
+  console.log("Kill Code");
+}
+
+//Sends Save code to another player
+function SaveCode (){
+  console.log("Save Code");
+}
+
+//Creates buttons to Kill and Save Players
+function CodeSend (props){
+
+  const numberID = props.id;
+
+  /*
+  if(numberID === 0 ){
+    numberID = 1;
+  }else{
+    numberID = 0;
+  }
+  
+  */
+
+  return(
+    < >
+      <p>Player: {playerArray[numberID].name} </p>
+      <input type="button" value="Kill" onClick={KillCode}></input> 
+      <input type="button" value="Save" onClick={SaveCode}></input>
+    </>
+  )
+}
+
+//Creates the each round's control inferface
+const RoundHTML = ({currentTeam, id}) => {
+
+  const newCodeWord = RedCodeWordsArray[0];
+
+  /* Will need to determine what team player is on
+
+  const [newCodeWord, setCodeWord] = useState("");
+
+  if({currentTeam} === "Red"){
+    setCodeWord(RedCodeWordsArray[0]);
+  }else{
+    if({currentTeam} === "Blue"){
+      setCodeWord(BlueCodeWordsArray[0]);
+    }
+  }
+  */
+
+  //Creates Round Info
+  return(
+    < >
+      <p>Current Team: {currentTeam}</p>
+
+      <p>Code Word: {newCodeWord}</p>
+
+      <CodeSend id={id}/>
+    </>
+  )
+}
+
+//Creates the results
+const ResultHTML = ({score}) => {
+  return(
+    < >
+      <p>Points This Round: {score}</p> 
+      <p>Total Points: {score}</p>
     </ >
   )
 }
 
 //Creates player profile interface
-function PlayerHTML (){
-
-    //Stores round value
-    const [nameVal, setNameVal] = useState("");
-
+const PlayerHTML = ({name, id, currentTeam, score}) => {
   return (
-    < >
-      <label> 
-        Name: <input type="text" value={nameVal} onChange={e => setNameVal(e.target.value)}></input>
-      </label>
 
-      <RoundHTML />
-        
-      <ResultHTML />
-    </ > 
-  )
+    < >      
+        <p>Name: {name}, id: {id}</p> 
+
+        <RoundHTML currentTeam={currentTeam} id={id}/>
+          
+        <ResultHTML score={score}/>
+
+        <p>----------------------------------------</p>
+
+    </ >
+    
+  );
+  
 } 
 
 
-//Creates the results
-function ResultHTML (){
-  return(
+const PlayerOverview = ({players}) => {
+
+  return (
     < >
-      <p>Points This Round: </p> 
-      <p>Total Points:</p>
+      <div>
+        {players.map(
+          (player, i) => 
+            <PlayerHTML 
+              key={i}
+              name={player.name} 
+              id={player.id}
+              currentTeam={player.currentTeam}
+              score={player.score} />
+        )}
+      </div>
     </ >
-  )
-}
+	)
 
-//Creates the each round's control inferface
-function RoundHTML (props){
-
-  //Creates Round Info
-  return(
-    < >
-      <p>Code Word:</p>
-
-      <CodeSend />
-    </>
-  )
 }
 
 //Compiles everything for final render
-function App(props) {
+function App() {
   return(
+
     < >
       <h1>Admin Interface:</h1>
       <AdminHTML />
 
       <br />
-      
+
       <h1>Player Interface:</h1>
-      <PlayerHTML />
+
+      <PlayerOverview players= {playerArray}/>
+     
     </ >
   )
 }
@@ -194,16 +286,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-/* Logic to be used later in having people create their own info
-<div>
-        {playerCreated === 0 ? (
-            <RoundHTML />
-            
-          ) : playerCreated === 1 ? (
-            <PlayerHTML />
-          ) : (
-            <h1>Error Please Reload</h1>
-        )}
-      </div>
-*/
